90c5b94f3cfa15d12c4c74b1879376fa4a96af47
Klimenkodan <45092039+Klimenkodan@users.noreply.github.com>
2019-10-17 01:56:00 +0300

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,7 +1,9 @@
 package ua.edu.ucu.tempseries;
 
 public final class TempSummaryStatistics {
+    
     public double avgTemp, devTemp, minTemp, maxTemp;
+    
     public TempSummaryStatistics(double avgTemp1, double devTemp1,
                                  double minTemp1, double maxTemp1) {
         avgTemp = avgTemp1;
@@ -9,4 +11,21 @@ public final class TempSummaryStatistics {
         maxTemp = maxTemp1;
         minTemp = minTemp1;
     }
+    
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+
+    public double getDevTemp() {
+        return devTemp;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
 }
+
